876038de92fdf3666aaf5ddfd955a6e8d2406e3c
Andy <aperez@bts.com.mx>
2023-01-20 13:12:30 -0800

    Fin Seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-import {AppComponent} from './app.component'
+import { AppComponent } from './app.component'
 import { MediaItemComponent } from "./media-item.component";
 import { MediaItemListComponent } from "./media-item-list.component";
 import { FavoriteDirective } from "./favorite.directive";
@@ -10,7 +10,7 @@ import { MediaItemFormComponent } from "./media-item-form.component";
 @NgModule({
      imports: [ //For modules ?
         BrowserModule,
-        FormsModule
+        ReactiveFormsModule
      ],
      declarations: [ //components, directives & pipes
         AppComponent,
